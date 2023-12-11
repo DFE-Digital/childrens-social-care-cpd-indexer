e2f2d64801ca5c4de0274d390e91c8ead2ea98e6
killij <51908793+killij@users.noreply.github.com>
2023-12-11 10:09:58 +0000

    feat: Add telemetry client (#17)
    * Update Program.cs
    
    * Update MockTelemetryClient.cs

--- src/Childrens-Social-Care-CPD-Indexer.Tests/MockTelemetryClient.cs
+++ src/Childrens-Social-Care-CPD-Indexer.Tests/MockTelemetryClient.cs
@@ -1,36 +1,20 @@
 ﻿using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
-using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Childrens_Social_Care_CPD_Indexer.Tests;
 
-public class MockTelemetryChannel : ITelemetryChannel
+internal sealed class MockTelemetryChannel : ITelemetryChannel
 {
-    public ConcurrentBag<ITelemetry> SentTelemtries = new ConcurrentBag<ITelemetry>();
+    public ConcurrentBag<ITelemetry> SentTelemtries = [];
     public bool IsFlushed { get; private set; }
     public bool? DeveloperMode { get; set; }
-    public string EndpointAddress { get; set; }
+    public string EndpointAddress { get; set; } = string.Empty;
 
-    public void Send(ITelemetry item)
-    {
-        this.SentTelemtries.Add(item);
-    }
-
-    public void Flush()
-    {
-        this.IsFlushed = true;
-    }
-
-    public void Dispose()
-    {
-
-    }
+    public void Send(ITelemetry item) => SentTelemtries.Add(item);
+    public void Flush() => IsFlushed = true;
+    public void Dispose() {}
 }
 
 internal static class MockTelemetryClient
